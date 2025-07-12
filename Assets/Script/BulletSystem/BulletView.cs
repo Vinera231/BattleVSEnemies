e9b7b771146de1627a7b2b8eaf85c8aa14b204e1
Vinera231 <zenderpro1610@gmail.com>
2025-07-12 21:42:24 +0300

    Улучшел патрони

--- Assets/Script/BulletSystem/BulletView.cs
+++ Assets/Script/BulletSystem/BulletView.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 public class BulletView : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private GameObject[] _bulletGui;
+    [SerializeField] private Animator _anim;
+   
+    [SerializeField] private float FirstTreshold = 66f;
+    [SerializeField] private float SecondTreshold = 33f;
+    [SerializeField] private float ThirdTreshold = 0f;
 
-    public void UpdateBulletCount(float count, float max) =>
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    public void UpdateBulletCount(float count, float max)
+    {
         _text.text = $" {count}";
 
+        if (_bulletGui.Length > 0)
+            _bulletGui[0].SetActive(count >= FirstTreshold);
+
+        if (_bulletGui.Length > 0)
+            _bulletGui[1].SetActive(count >= SecondTreshold);
+
+        if (_bulletGui.Length > 0)
+            _bulletGui[2].SetActive(count >= SecondTreshold);
+    }
+
+    public void BulletNotAnimation()
+    {
+        if (_anim != null)
+        {
+            _anim.Play("NotBullets");
+        }
+    }
 }
--- Assets/Script/BulletSystem/BulletViewAnim.cs
+++ Assets/Script/BulletSystem/BulletViewAnim.cs
@@ -4,20 +4,12 @@ using UnityEngine;
 public class BulletViewAnim : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _text;
-    [SerializeField] private Animator _anim;
-    private void Start()
-    {
-        _anim = GetComponent<Animator>();
-    }
+   
    
     public void BulletNotAnimation(float count,float max)
     {
         _text.text = $"{count}";
    
-        if(_anim != null)
-        {
-            _anim.Play("NotBullets");
-        }
 
     }
 }
