a940f2352c6ba6f0701040f60aec547fae74fe90
Vinera231 <zenderpro1610@gmail.com>
2025-09-18 16:51:17 +0300

    Добавил настройки чуствительности в настройках

--- Assets/Script/Camera/CameraFollower.cs
+++ Assets/Script/Camera/CameraFollower.cs
@@ -1,14 +1,15 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
+ 
     [SerializeField] private Transform _target;
     [SerializeField] private Transform _playerBody;
     [SerializeField] private float _mouseSensitivity = 330f;
     [SerializeField] private float _smoothRotation = 20f;
 
     private float _xRotation = 0f;
+
     private void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity * Time.deltaTime;
@@ -19,15 +20,25 @@ public class CameraFollower : MonoBehaviour
         _xRotation -= mouseY;
         _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
         _target.localRotation = Quaternion.Euler(_xRotation,0f, 0f);
-    
     }
 
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("MouseSensetive"))
+            _mouseSensitivity = PlayerPrefs.GetFloat("MouseSensetive");
+    }
     private void LateUpdate()
     {
         if (_target != null)
         {
           transform.SetPositionAndRotation(_target.position, _target.rotation);
-          transform.position = Vector3.Lerp(transform.position,_target.position,_smoothRotation* Time.deltaTime);
+          transform.position = Vector3.Lerp(transform.position,
+         _target.position,_smoothRotation* Time.deltaTime);
         }
+    }
+  
+    public void SetSensetive(float value)
+    {       
+        _mouseSensitivity = value;
     }
 }
---
+++ Assets/Script/Camera/CameraSetting.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraSetting : MonoBehaviour
+{
+  
+    [Header("Reference")]
+    [SerializeField] private Slider _sensetiveSlider;
+    [SerializeField] private CameraFollower _follower;
+    [SerializeField] private Transform _playerBody;
+
+    [Header("Setting")]
+    [SerializeField] private float _minSensitive = 250f;
+    [SerializeField] private float _maxSensitive = 500f;
+    [SerializeField] private float _defaultSensitivity = 330f;
+
+
+    private void Start()
+    {
+        float savedSensitive = PlayerPrefs.GetFloat("MouseSensitive", _defaultSensitivity);
+
+        if (_sensetiveSlider)
+        {
+            _sensetiveSlider.minValue = _minSensitive;
+            _sensetiveSlider.maxValue = _maxSensitive;
+            _sensetiveSlider.value = savedSensitive;
+
+            _sensetiveSlider.onValueChanged.AddListener(UpdateSensitive);
+        }
+    
+        if(_follower)
+            _follower.SetSensetive(savedSensitive);
+    }
+
+     public void UpdateSensitive(float value)
+    {
+        PlayerPrefs.GetFloat("MouseSensitive", value);
+
+        if (_follower)
+            _follower.SetSensetive(value);
+    }
+
+}
