53a640d74400ba6f68b0b7f6819d3005500e6ca0
Vinera231 <zenderpro1610@gmail.com>
2025-09-30 17:14:12 +0300

    Добавыл  звук обычний после босса изменил цену:морозной пушки,зелье,буллет бег

--- Assets/Script/Audio/Music.cs
+++ Assets/Script/Audio/Music.cs
@@ -6,12 +6,20 @@ public class Music : MonoBehaviour
     [SerializeField] private AudioClip _defaultMusic;
     [SerializeField] private AudioClip _bossMusic;
     [SerializeField] private Wave _beforeBoss;
+    [SerializeField] private Wave _afterBoss;
 
-    private void OnEnable() =>
-        _beforeBoss.Finished += PlayBossMusic;
+    private void OnEnable()
+    {
+     _beforeBoss.Finished += PlayBossMusic;
+     _afterBoss.Finished += PlayDefaultMusic;
+    }
+        
 
-    private void OnDisable() =>
-        _beforeBoss.Finished -= PlayBossMusic;
+    private void OnDisable()
+    {
+     _beforeBoss.Finished -= PlayBossMusic;
+     _afterBoss.Finished -= PlayDefaultMusic;
+    }        
 
     public void PlayBossMusic() =>
         Play(_bossMusic);
