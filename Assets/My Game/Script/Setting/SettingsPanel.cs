60a9f51890530b0fc5dfae221b8eaeea99622cde
Vinera231 <zenderpro1610@gmail.com>
2025-12-05 12:59:10 +0200

    Исправил проблеми в игре

--- Assets/My Game/Script/Setting/SettingsPanel.cs
+++ Assets/My Game/Script/Setting/SettingsPanel.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class SettingsPanel : MonoBehaviour
 {
     [SerializeField] private PauseSwitcher _pauseSwitcher;
+    [SerializeField] private ButtonClosePanel _closePanel;
 
-    private void OnEnable() =>
+    private void OnEnable()
+    {
         _pauseSwitcher.PauseGame(gameObject);
+        _closePanel.PanelClosed += Hide;
+    }
 
-    private void OnDisable() =>
+    private void OnDisable()
+    {
         _pauseSwitcher.PlayGame(gameObject);
+        _closePanel.PanelClosed -= Hide;
+    }
 
     public void Show() =>
         gameObject.SetActive(true);
